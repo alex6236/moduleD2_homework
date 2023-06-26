0445efeb48cec16d23b5b88e81c17cbb8a319d63
Alex <sova11@mail.ru>
2023-06-27 02:32:59 +0300

    Финиш

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import NewsList, NewsDetail
 
 urlpatterns = [
-    # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно почему
-    path('', NewsList.as_view()),
-    path('<int:pk>', NewsDetail.as_view()),
+    path('', NewsList.as_view(), name='news'),
+    path('<int:pk>', NewsDetail.as_view(), name='news_detail'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,14 +1,22 @@
-# from django.shortcuts import render
-# импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
+
+from datetime import datetime, timezone
 from django.views.generic import ListView, DetailView
 from .models import Post
 
 class NewsList(ListView):
     model = Post
-    template_name ='news.html'
+    template_name = 'news.html'
     context_object_name = 'news'
+    queryset = Post.objects.order_by('-id')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['time_now'] = datetime.utcnow()
+        context['time_now'] = datetime.now(timezone.utc) 
+        # context['value1'] = None
+        return context
     
 class NewsDetail(DetailView):
     model = Post
-    template_name ='news_id.html'
-    context_object_name = 'news_id'
+    template_name ='news_detail.html'
+    context_object_name = 'news_detail'
